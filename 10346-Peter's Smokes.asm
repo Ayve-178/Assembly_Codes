e067072d1b847ea65e1d002338ae227290128938
Ayve-178 <49992828+Ayve-178@users.noreply.github.com>
2020-09-04 01:30:21 +0600

    Create 10346-Peter's Smokes.asm

---
+++ 10346-Peter's Smokes.asm
@@ -0,0 +1,92 @@
+.MODEL SMALL
+.STACK 100H
+.DATA  
+    R DW ?
+    T DW ? 
+    Z DW ?
+    A DW ?
+    N DW ?
+    K DW ?
+.CODE
+MAIN PROC
+    MOV AX,@DATA
+    MOV DS,AX    
+    CALL TAKING_INPUT
+    MOV N,AX  
+    MOV A,AX
+    MOV T,AX      
+    CALL NEWLINE 
+    CALL TAKING_INPUT
+    MOV K,AX        
+    CALL NEWLINE   
+    LEVEL:  
+        MOV CX,A
+        CMP CX,K
+        JL OUTPUT
+        MOV AX,A
+        MOV BX,K 
+        MOV DX,0
+        DIV BX  
+        MOV R,AX ;R=AX=AX/BX  
+        ADD AX,T
+        MOV T,AX
+        ADD DX,R
+        MOV A,DX
+        JMP LEVEL
+    OUTPUT:
+        MOV AX,T
+        CALL PRINTING_OUTPUT            
+    EXIT:
+        MOV AH,4CH
+        INT 21H     
+    MAIN ENDP  
+
+NEWLINE PROC 
+        MOV AH,2
+        MOV DL,10
+        INT 21H
+        MOV DL,13
+        INT 21H
+        RET            
+    NEWLINE ENDP
+
+TAKING_INPUT PROC
+        MOV CX,0
+        INPUT:
+        MOV AH,1
+        INT 21H
+        CMP AL,13
+        JNE CALC
+        MOV AX,CX 
+        RET        
+        CALC:
+        SUB AL,48
+        MOV AH,0
+        MOV BX,AX        
+        MOV DX,10
+        MOV AX,CX
+        MUL DX
+        ADD AX,BX        
+        MOV CX,AX
+        JMP INPUT    
+    TAKING_INPUT ENDP  
+
+PRINTING_OUTPUT PROC
+        MOV CX,0        
+        PRINT:
+        MOV DX,0
+        MOV BX,10
+        DIV BX
+        PUSH DX
+        INC CX
+        CMP AX,0
+        JNE PRINT        
+        PRINTF:
+        MOV AH,2
+        POP DX
+        ADD DL,48
+        INT 21H
+        LOOP PRINTF        
+        RET        
+    PRINTING_OUTPUT ENDP
+END MAIN
