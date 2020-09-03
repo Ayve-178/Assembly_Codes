45661c5b1dc239896d338824dc5c32c4c23645a9
Ayve-178 <49992828+Ayve-178@users.noreply.github.com>
2020-09-04 01:25:37 +0600

    Create 13012-Identifying tea.asm

---
+++ 13012-Identifying tea.asm
@@ -0,0 +1,91 @@
+.MODEL SMALL
+.STACK 100H
+.DATA 
+    T DW ?
+    N DW ?
+    X DW ?
+    SUM DW ?
+.CODE
+MAIN PROC
+    MOV AX,@DATA
+    MOV DS,AX 
+    MOV SUM,0 
+    CALL TAKING_INPUT 
+    MOV N,AX
+    CALL NEWLINE  
+    MOV T,5     
+    MOV CX,T
+    INC CX
+    MOV T,CX
+    LEVEL: 
+        MOV CX,T
+        DEC CX
+        MOV T,CX
+        CMP CX,0
+        JE OUTPUT
+        CALL TAKING_INPUT
+        MOV X,AX       ;NUMBER
+        CALL NEWLINE    
+        MOV AX,X       ;IF(NUMBER==N)
+        CMP AX,N
+        JE CAL          
+        JMP LEVEL 
+    CAL:
+        MOV BX,SUM
+        INC SUM
+        JMP LEVEL
+    OUTPUT:
+        MOV AX,SUM
+        CALL PRINTING_OUTPUT
+        CALL NEWLINE     
+    EXIT:
+        MOV AH,4CH
+        INT 21H         
+    MAIN ENDP  
+NEWLINE PROC 
+        MOV AH,2
+        MOV DL,10
+        INT 21H
+        MOV DL,13
+        INT 21H
+        RET            
+    NEWLINE ENDP 
+TAKING_INPUT PROC
+        MOV CX,0
+        INPUT:
+        MOV AH,1
+        INT 21H
+        CMP AL,13
+        JNE CALC
+        MOV AX,CX 
+        RET
+        CALC:
+        SUB AL,48
+        MOV AH,0
+        MOV BX,AX
+        MOV DX,10
+        MOV AX,CX
+        MUL DX
+        ADD AX,BX        
+        MOV CX,AX
+        JMP INPUT    
+    TAKING_INPUT ENDP  
+PRINTING_OUTPUT PROC
+        MOV CX,0        
+        PRINT:
+        MOV DX,0
+        MOV BX,10
+        DIV BX
+        PUSH DX
+        INC CX
+        CMP AX,0
+        JNE PRINT        
+        PRINTF:
+        MOV AH,2
+        POP DX
+        ADD DL,48
+        INT 21H
+        LOOP PRINTF        
+        RET        
+    PRINTING_OUTPUT ENDP
+END MAIN
